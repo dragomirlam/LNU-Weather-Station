9154fe141a232691f245c8e943ccf8937107a826
Lampros Dragomoiris <dragomirlam@gmail.com>
2014-09-09 18:53:38 +0200

    Install and config Google's map

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
     	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     	<link rel="stylesheet" href="css/foundation.css" />
     	<script src="js/vendor/modernizr.js"></script>
+    	
+    	<!-- Google Map -->
+    	<style>
+      		#map_canvas {
+        		width: 500px;
+        		height: 400px;
+      		}
+    	</style>
+    	<script src="https://maps.googleapis.com/maps/api/js"></script>
     </head>
     <body>
     	<h1>Linnaeus University - Netatmo Weather Station</h1>
@@ -44,9 +53,20 @@
 			    if(isset($deviceList["devices"][0]))
 			    {
 			        $device_id = $deviceList["devices"][0]["_id"];
+			        $station_name = $deviceList["devices"][0]["station_name"];
+			        $firmware = $deviceList["devices"][0]["firmware"];
+			        $wifi_status = $deviceList["devices"][0]["wifi_status"];
+			        $place = $deviceList["devices"][0]["place"];
+			        $lon = $place["location"][0];
+			        $lat = $place["location"][1];
+
+			        echo "<h2>".$device_id."</h2>";
+			        echo "<h2>".$firmware."</h2>";
+			        echo "<h2>".$place."</h2>";
+
 			        // Ok display dashboard_data
 			        if(isset($deviceList["devices"][0]["dahsboard_data"]))
-			            print_r($deviceList["devices"][0]["dashboard_data"]);
+			            echo "<h4>".$deviceList["devices"][0]["dashboard_data"]."</h4>";
 			        // Ok now retrieve current mean day temperature,humidity
 			        $params = array("scale" =>"max",
 			                        "type"=>"Temperature,Humidity,Co2,Pressure,Noise",
@@ -68,7 +88,7 @@
 			            echo "<h4>Co2 is $c ppm</h4>";
 			            echo "<h4>Pressure is $p mbar</h4>";
 			            echo "<h4>Noise is $n db</h4>";
-			        } 
+			        }
 			    }
 			}
 			catch(NAClientException $ex)
@@ -76,6 +96,23 @@
 			    echo "User does not have any devices\n";
 			}
 		?>
+		<script>
+      		function initialize() {
+      			//Assign the variable from the previous php code to the javascript ones!
+      			var lon = '<?php echo $lon; ?>';
+      			var lat = '<?php echo $lat; ?>';
+        		var mapCanvas = document.getElementById('map_canvas');
+        		var mapOptions = {
+        				//configure the google map latitude and longtitude
+          				center: new google.maps.LatLng(lat, lon),
+          				zoom: 8,
+          				mapTypeId: google.maps.MapTypeId.ROADMAP
+        			}
+        		var map = new google.maps.Map(mapCanvas, mapOptions)
+      		}
+      		google.maps.event.addDomListener(window, 'load', initialize);
+    	</script>
+		<div id="map_canvas"></div>
 
 		<!-- Foundation -->
 		<script src="js/vendor/jquery.js"></script>
